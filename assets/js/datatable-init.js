923b1319ca5f321f760575a0fa4e2e09c2e489b9
windyvpwidianto <windyvpwidianto@gmail.com>
2026-07-29 14:34:49 +0800

    5er9

--- assets/js/datatable-init.js
+++ assets/js/datatable-init.js
@@ -1,12 +1,24 @@
 $(document).ready(function () {
 
     console.log("JQUERY =", typeof $);
-
     console.log("DATATABLE =", typeof $.fn.DataTable);
 
-    $('#employeesTable').DataTable({
-        pageLength:10,
-        responsive:true
-    });
+    // Employees
+    if ($('#employeesTable').length) {
+        $('#employeesTable').DataTable({
+            pageLength: 10,
+            responsive: true,
+            autoWidth: false
+        });
+    }
+
+    // Semua tabel yang memakai class .datatable
+    if ($('.datatable').length) {
+        $('.datatable').DataTable({
+            pageLength: 10,
+            responsive: true,
+            autoWidth: false
+        });
+    }
 
 });
