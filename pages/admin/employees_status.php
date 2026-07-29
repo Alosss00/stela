de7332432db6548d1b8bb30528ee0f7fef41ff2d
windyvpwidianto <windyvpwidianto@gmail.com>
2026-07-29 10:40:56 +0800

    disjpo

--- pages/admin/employees_status.php
+++ pages/admin/employees_status.php
@@ -197,7 +197,7 @@ WHERE is_active=0
         <div class="card-body-emp">
             <?php if ($employees->num_rows > 0): ?>
                 <div class="table-responsive">
-                    <table class="table-emp" id="employeesTable">
+                    <table class="table-emp datatable" id="employeesTable">
                         <thead>
                             <tr>
                                 <th class="col-code" data-lang="id-badge">ID BADGE</th>
