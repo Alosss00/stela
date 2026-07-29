dec08f4fa635a924341c82893aceafc44c49b24b
windyvpwidianto <windyvpwidianto@gmail.com>
2026-07-29 14:56:38 +0800

    iygf

--- assets/js/datatable-init.js
+++ assets/js/datatable-init.js
@@ -3,9 +3,19 @@ $(document).ready(function () {
     console.log("JQUERY =", typeof $);
     console.log("DATATABLE =", typeof $.fn.DataTable);
 
-    $('#employeesTable').DataTable({
-        pageLength: 10,
-        responsive: true
+    // Inisialisasi semua tabel yang mempunyai class .datatable
+    $('.datatable').each(function () {
+
+        if (!$.fn.DataTable.isDataTable(this)) {
+
+            $(this).DataTable({
+                pageLength: 10,
+                responsive: true,
+                autoWidth: false
+            });
+
+        }
+
     });
 
 });
