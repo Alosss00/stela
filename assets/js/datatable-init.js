c622ec0f5fd87bf4910b8220304a14cb025c70b5
windyvpwidianto <windyvpwidianto@gmail.com>
2026-07-30 14:22:46 +0800

    wgsdhik

--- assets/js/datatable-init.js
+++ assets/js/datatable-init.js
@@ -1,8 +1,5 @@
 $(document).ready(function () {
 
-    console.log("JQUERY =", typeof $);
-    console.log("DATATABLE =", typeof $.fn.DataTable);
-
     // Inisialisasi semua tabel yang mempunyai class .datatable
     $('.datatable').each(function () {
 
@@ -11,7 +8,24 @@ $(document).ready(function () {
             $(this).DataTable({
                 pageLength: 10,
                 responsive: true,
-                autoWidth: false
+                autoWidth: false,
+                dom: "<'dt-controls-top'<'dt-length-container'l><'dt-search-container'f>>" +
+                     "<'table-responsive'tr>" +
+                     "<'dt-controls-bottom'<'dt-info-container'i><'dt-paginate-container'p>>",
+                language: {
+                    lengthMenu: "Tampilkan _MENU_ data",
+                    info: "Menampilkan _START_ - _END_ dari _TOTAL_ data",
+                    infoEmpty: "Menampilkan 0 data",
+                    infoFiltered: "(disaring dari _MAX_ total data)",
+                    zeroRecords: "Tidak ada data yang ditemukan",
+                    search: "Cari:",
+                    paginate: {
+                        first: '<i class="fas fa-angle-double-left"></i>',
+                        previous: '<i class="fas fa-angle-left"></i>',
+                        next: '<i class="fas fa-angle-right"></i>',
+                        last: '<i class="fas fa-angle-double-right"></i>'
+                    }
+                }
             });
 
         }
