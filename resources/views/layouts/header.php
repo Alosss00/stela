c8e819982dd795797826dcb9c9c672f43567c818
windyvpwidianto <windyvpwidianto@gmail.com>
2026-07-31 13:42:40 +0800

    ubah warna

--- resources/views/layouts/header.php
+++ resources/views/layouts/header.php
@@ -42,7 +42,7 @@ $current_page = get_current_page();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($page_title) ? $page_title . ' - ' : ''; ?><?php echo SITE_NAME; ?></title>
-    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/assets/css/style.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/assets/css/style.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>/assets/css/language-switcher.css">
     <link rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
