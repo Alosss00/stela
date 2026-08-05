ed2b63b2d3fedcafae09fe6d47feadd7fe1001ec
windyvpwidianto <windyvpwidianto@gmail.com>
2026-08-05 09:20:12 +0800

    hjf

--- resources/views/admin/migrate_db.php
+++ resources/views/admin/migrate_db.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once dirname(__DIR__, 2) . '/app/Helpers/auth_helper.php';
+require_once dirname(__DIR__, 3) . '/app/Helpers/auth_helper.php';
 $db = new Database();
 
 echo "<h2>Database Migration</h2>";
