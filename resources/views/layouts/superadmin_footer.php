8691ac635ab23f4836d1807281afce92c3400743
windyvpwidianto <windyvpwidianto@gmail.com>
2026-08-11 15:13:22 +0800

    verify_employee

--- resources/views/layouts/superadmin_footer.php
+++ resources/views/layouts/superadmin_footer.php
@@ -3,6 +3,8 @@
     </div>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>/assets/js/script.js"></script>
+    <script src="<?php echo BASE_URL; ?>/assets/js/language-switcher.js"></script>
     <script src="https://cdn.datatables.net/1.13.8/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.8/js/dataTables.bootstrap5.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
