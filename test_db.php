df81397d0df5639faefbaf347839b91a4be33f02
windyvpwidianto <windyvpwidianto@gmail.com>
2026-08-19 15:43:04 +0800

    fsds

--- test_db.php
+++ test_db.php
@@ -1,7 +1,9 @@
 <?php
 require_once __DIR__ . '/bootstrap/app.php';
 $db = new Database();
-$res = $db->query('SHOW TABLES');
-while($row = $res->fetch_array()) {
-    echo $row[0] . "\n";
+$res = $db->query("ALTER TABLE employees MODIFY COLUMN supervision_area VARCHAR(100) NULL");
+if ($res) {
+    echo 'Success';
+} else {
+    echo $db->getConnection()->error;
 }
