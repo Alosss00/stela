515afca7aa0c5d1106cafccca654f889aa3a9b61
windyvpwidianto <windyvpwidianto@gmail.com>
2026-08-27 15:18:13 +0800

    config

--- config/app.php
+++ config/app.php
@@ -8,7 +8,8 @@ $envFile = dirname(__DIR__) . '/.env';
 if (file_exists($envFile)) {
     $lines = file($envFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
     foreach ($lines as $line) {
-        if (strpos(trim($line), '#') === 0) continue;
+        $line = trim($line);
+        if (strpos($line, '#') === 0 || strpos($line, '=') === false) continue;
         list($name, $value) = explode('=', $line, 2);
         $name = trim($name);
         $value = trim($value);
