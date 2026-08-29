b6664944e376f7910b701504f1facab2f374d455
windyvpwidianto <windyvpwidianto@gmail.com>
2026-08-29 16:18:48 +0800

    Perbaikan struktur folder

--- scratch/check_cols.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'bootstrap/app.php';
-$db = new Database();
-$res = $db->query('DESCRIBE departments');
-while($row = $res->fetch_assoc()) echo $row['Field'] . ' ';
-echo "\n";
-$res = $db->query('DESCRIBE companies');
-while($row = $res->fetch_assoc()) echo $row['Field'] . ' ';
-echo "\n";
